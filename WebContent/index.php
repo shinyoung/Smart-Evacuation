d5f0384040b34a040fbddd7ea26571178e2d3f6f
shinyoung <beethoven@hanyang.ac.kr>
2014-05-30 01:26:10 +0900

    Update index.php

--- WebContent/index.php
+++ WebContent/index.php
@@ -11,14 +11,19 @@
     exit;
   }
 
+$name = $_POST[name];
+$query = "select * from PEOPLE where name='$name'";
+$result = mysql_query($query, $connect);
+if(mysql_num_rows($result) != 0){
+ echo "<script>alert(\"이름이 중복됩니다.\"); history.back(-1);</script>";
+}else {
   $query = "insert into PEOPLE values(null,'".$_POST["name"]."', ".$_POST["age"].", '".$_POST["sex"]."', '".$_POST["defect"]."', null)";
 
-
-
   $result = mysql_query($query, $connect);
-  if($result) echo "<meta http-equiv='refresh' content='0; url=http://smartev.dothome.co.kr/WebContent/building.html'>";
+  if($result)  echo "<meta http-equiv='refresh' content='0;url=http://smartev.dothome.co.kr/WebContent/building.html?name=$name'>";
   else echo "Input Failed<br>";
   echo "<hr>";
+}
   ?>
   <input type=submit value="Back">
   </form>
